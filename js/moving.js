e3cefaa9a85ad4a1784ff4f4f7e9224ad0e3c7ed
garajnik <lexan1699@gmail.com>
2023-10-03 12:33:35 +0800

    Reconstruction to business card

---
+++ js/moving.js
@@ -0,0 +1,15 @@
+const container = document.querySelector(".container");
+const body = document.body;
+
+body.addEventListener("mousemove", (event) => {
+  const mouseX = event.clientX;
+  const mouseY = event.clientY;
+
+  const offsetX = window.innerWidth / 2 - mouseX;
+  const offsetY = window.innerHeight / 2 - mouseY;
+
+  const percentX = (mouseX / window.innerWidth) * 100;
+  body.style = `background: linear-gradient(${percentX}deg, rgba(48,47,60,1) 0%, rgba(90,68,99,1) 100%);`;
+
+  container.style.transform = `translate(${offsetX / 30}px, ${offsetY / 30}px)`;
+});
