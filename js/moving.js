6347325fc30df1c29c0bd948f6b40159c12273fe
garajnik <lexan1699@gmail.com>
2023-10-03 13:10:04 +0800

    Added buttons

--- js/moving.js
+++ js/moving.js
@@ -13,3 +13,22 @@ body.addEventListener("mousemove", (event) => {
 
   container.style.transform = `translate(${offsetX / 30}px, ${offsetY / 30}px)`;
 });
+
+var firstButton = document.getElementById("3D");
+var secondButton = document.getElementById("Projects");
+var thirdButton = document.getElementById("YouTube");
+
+firstButton.addEventListener("click", function () {
+  window.open("https://www.artstation.com/garajnik", "_blank");
+});
+
+secondButton.addEventListener("click", function () {
+  window.open("https://github.com/Garajnik", "_blank");
+});
+
+thirdButton.addEventListener("click", function () {
+  window.open(
+    "https://www.youtube.com/channel/UCDLOBx52mxQfFNkl9R14_yw",
+    "_blank"
+  );
+});
