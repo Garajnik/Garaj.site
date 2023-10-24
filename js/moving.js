cc9e4c3f2eaf85c919df3995026c40ca81f2acff
garajnik <lexan1699@gmail.com>
2023-10-24 18:06:16 +0800

    11

--- js/moving.js
+++ js/moving.js
@@ -1,19 +1,3 @@
-const container = document.querySelector(".container");
-const body = document.body;
-
-body.addEventListener("mousemove", (event) => {
-  const mouseX = event.clientX;
-  const mouseY = event.clientY;
-
-  const offsetX = window.innerWidth / 2 - mouseX;
-  const offsetY = window.innerHeight / 2 - mouseY;
-
-  const percentX = (mouseX / window.innerWidth) * 100;
-  body.style = `background: linear-gradient(${percentX}deg, rgba(48,47,60,1) 0%, rgba(90,68,99,1) 100%);`;
-
-  container.style.transform = `translate(${offsetX / 30}px, ${offsetY / 30}px)`;
-});
-
 var firstButton = document.getElementById("3D");
 var secondButton = document.getElementById("Projects");
 var thirdButton = document.getElementById("YouTube");
